7002447fe9037a450fc5765c51079f5cb97a79e8
teymureyvaz <43016690+teymureyvaz@users.noreply.github.com>
2018-12-07 02:36:13 +0400

    Update wsgi.py

--- release_date/wsgi.py
+++ release_date/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'release_date.settings')
