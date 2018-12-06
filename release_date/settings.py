5fd279b4f3d66f0489b89fc288b7cf1138e0ed54
teymureyvaz <43016690+teymureyvaz@users.noreply.github.com>
2018-12-07 02:39:26 +0400

    Update settings.py

--- release_date/settings.py
+++ release_date/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
